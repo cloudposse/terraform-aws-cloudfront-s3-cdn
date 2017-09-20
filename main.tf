a8fb35a2688b321e70b730ab5f58257ae18964b7
Erik Osterman <erik@cloudposse.com>
2017-09-20 15:25:33 -0700

    Update new terraform-aws-route53-alias module

--- main.tf
+++ main.tf
@@ -162,7 +162,7 @@ resource "aws_cloudfront_distribution" "default" {
 }
 
 module "dns" {
-  source           = "git::https://github.com/cloudposse/tf_vanity.git?ref=tags/0.2.1"
+  source           = "git::https://github.com/cloudposse/terraform-aws-route53-alias.git?ref=tags/0.2.2"
   aliases          = "${var.aliases}"
   parent_zone_id   = "${var.parent_zone_id}"
   parent_zone_name = "${var.parent_zone_name}"
