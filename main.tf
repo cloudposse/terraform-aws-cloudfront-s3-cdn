37b59619e7bad06c88b1e7e9ecdb1849601c1aaf
aknysh <aknysh>
2018-02-15 12:45:08 -0500

    Update `cloudfront_default_certificate` attribute

--- main.tf
+++ main.tf
@@ -128,7 +128,7 @@ resource "aws_cloudfront_distribution" "default" {
     acm_certificate_arn            = "${var.acm_certificate_arn}"
     ssl_support_method             = "sni-only"
     minimum_protocol_version       = "TLSv1"
-    cloudfront_default_certificate = true
+    cloudfront_default_certificate = "${var.acm_certificate_arn == "" ? true : false}"
   }
 
   default_cache_behavior {
