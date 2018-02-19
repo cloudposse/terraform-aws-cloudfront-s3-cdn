4156d0ed58f137e0b449aa1adcf574e076fdf843
aknysh <andriy.knysh@gmail.com>
2018-02-19 10:03:47 -0500

    Bump `terraform-null-label` version to `0.3.1`

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "origin_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.2.1"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.1"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"
@@ -79,7 +79,7 @@ module "logs" {
 }
 
 module "distribution_label" {
-  source    = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.2.1"
+  source    = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.1"
   namespace = "${var.namespace}"
   stage     = "${var.stage}"
   name      = "${var.name}"
