d583baecfe1e46e4290cd99944ca66618b31abae
vadim-hleif <vadzim.hleif@gmail.com>
2018-07-04 17:16:34 +0300

    Migrate to README.yaml format

--- variables.tf
+++ variables.tf
@@ -32,7 +32,8 @@ variable "tags" {
 }
 
 variable "enabled" {
-  default = "true"
+  default     = "true"
+  description = "State of CloudFront"
 }
 
 variable "acm_certificate_arn" {
@@ -53,7 +54,8 @@ variable "use_regional_s3_endpoint" {
 }
 
 variable "origin_bucket" {
-  default = ""
+  default     = ""
+  description = "Name of S3 bucket"
 }
 
 variable "origin_path" {
@@ -63,35 +65,43 @@ variable "origin_path" {
 }
 
 variable "origin_force_destroy" {
-  default = "false"
+  default     = "false"
+  description = "Delete all objects from the bucket  so that the bucket can be destroyed without error (e.g. `true` or `false`)"
 }
 
 variable "bucket_domain_format" {
-  default = "%s.s3.amazonaws.com"
+  default     = "%s.s3.amazonaws.com"
+  description = "Format of bucket domain name"
 }
 
 variable "compress" {
-  default = "false"
+  default     = "false"
+  description = "Compress content for web requests that include Accept-Encoding: gzip in the request header"
 }
 
 variable "is_ipv6_enabled" {
-  default = "true"
+  default     = "true"
+  description = "State of CloudFront IPv6"
 }
 
 variable "default_root_object" {
-  default = "index.html"
+  default     = "index.html"
+  description = "Object that CloudFront return when requests the root URL"
 }
 
 variable "comment" {
-  default = "Managed by Terraform"
+  default     = "Managed by Terraform"
+  description = "Comment for the origin access identity"
 }
 
 variable "log_include_cookies" {
-  default = "false"
+  default     = "false"
+  description = "Include cookies in access logs"
 }
 
 variable "log_prefix" {
-  default = ""
+  default     = ""
+  description = "Path of logs in S3 bucket"
 }
 
 variable "log_standard_transition_days" {
@@ -110,39 +120,47 @@ variable "log_expiration_days" {
 }
 
 variable "forward_query_string" {
-  default = "false"
+  default     = "false"
+  description = "Forward query strings to the origin that is associated with this cache behavior"
 }
 
 variable "cors_allowed_headers" {
-  type    = "list"
-  default = ["*"]
+  type        = "list"
+  default     = ["*"]
+  description = "List of allowed headers  for S3 bucket"
 }
 
 variable "cors_allowed_methods" {
-  type    = "list"
-  default = ["GET"]
+  type        = "list"
+  default     = ["GET"]
+  description = "List of allowed methods (e.g. ` GET, PUT, POST, DELETE, HEAD`) for S3 bucket"
 }
 
 variable "cors_allowed_origins" {
-  type    = "list"
-  default = []
+  type        = "list"
+  default     = []
+  description = "List of allowed origins (e.g. ` example.com, test.com`) for S3 bucket"
 }
 
 variable "cors_expose_headers" {
-  type    = "list"
-  default = ["ETag"]
+  type        = "list"
+  default     = ["ETag"]
+  description = "List of expose header in the response for S3 bucket"
 }
 
 variable "cors_max_age_seconds" {
-  default = "3600"
+  default     = "3600"
+  description = "Time in seconds that browser can cache the response for S3 bucket"
 }
 
 variable "forward_cookies" {
-  default = "none"
+  default     = "none"
+  description = "Time in seconds that browser can cache the response for S3 bucket"
 }
 
 variable "price_class" {
-  default = "PriceClass_100"
+  default     = "PriceClass_100"
+  description = "Price class for this distribution: `PriceClass_All`, `PriceClass_200`, `PriceClass_100`"
 }
 
 variable "viewer_protocol_policy" {
@@ -151,45 +169,54 @@ variable "viewer_protocol_policy" {
 }
 
 variable "allowed_methods" {
-  type    = "list"
-  default = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+  type        = "list"
+  default     = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
+  description = "List of allowed methods (e.g. ` GET, PUT, POST, DELETE, HEAD`) for AWS CloudFront"
 }
 
 variable "cached_methods" {
-  type    = "list"
-  default = ["GET", "HEAD"]
+  type        = "list"
+  default     = ["GET", "HEAD"]
+  description = "List of cached methods (e.g. ` GET, PUT, POST, DELETE, HEAD`)"
 }
 
 variable "default_ttl" {
-  default = "60"
+  default     = "60"
+  description = "Default amount of time (in seconds) that an object is in a CloudFront cache"
 }
 
 variable "min_ttl" {
-  default = "0"
+  default     = "0"
+  description = "Minimum amount of time that you want objects to stay in CloudFront caches"
 }
 
 variable "max_ttl" {
-  default = "31536000"
+  default     = "31536000"
+  description = "Maximum amount of time (in seconds) that an object is in a CloudFront cache"
 }
 
 variable "geo_restriction_type" {
   # e.g. "whitelist"
-  default = "none"
+  default     = "none"
+  description = "Method that use to restrict distribution of your content by country: `none`, `whitelist`, or `blacklist`"
 }
 
 variable "geo_restriction_locations" {
   type = "list"
 
   # e.g. ["US", "CA", "GB", "DE"]
-  default = []
+  default     = []
+  description = "List of country codes for which  CloudFront either to distribute content (whitelist) or not distribute your content (blacklist)"
 }
 
 variable "parent_zone_id" {
-  default = ""
+  default     = ""
+  description = "ID of the hosted zone to contain this record  (or specify `parent_zone_name`)"
 }
 
 variable "parent_zone_name" {
-  default = ""
+  default     = ""
+  description = "Name of the hosted zone to contain this record (or specify `parent_zone_id`)"
 }
 
 variable "null" {
