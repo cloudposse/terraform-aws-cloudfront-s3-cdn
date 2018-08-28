0d591acb4e24cca60accc7a985daa665148172b3
Jamie Nelson <jamie@theinstillery.com>
2018-08-28 09:31:05 +0100

    Moved the forwarded header values into a var

--- main.tf
+++ main.tf
@@ -144,7 +144,7 @@ resource "aws_cloudfront_distribution" "default" {
 
     forwarded_values {
       query_string = "${var.forward_query_string}"
-      headers      = ["Access-Control-Request-Headers", "Access-Control-Request-Method", "Origin"]
+      headers      = ["${var.forward_header_values}"]
 
       cookies {
         forward = "${var.forward_cookies}"
