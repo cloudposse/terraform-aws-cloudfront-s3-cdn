3645642bba1d7418e975a1b957649721b1cd854a
maartenvanderhoef <maarten@blinkist.com>
2018-11-02 13:12:29 +0100

    *.bucket is evaluated before creation, changing to *.id

--- main.tf
+++ main.tf
@@ -97,7 +97,7 @@ data "aws_s3_bucket" "selected" {
 }
 
 locals {
-  bucket             = "${join("", compact(concat(list(var.origin_bucket), concat(list(""), aws_s3_bucket.origin.*.bucket))))}"
+  bucket             = "${join("", compact(concat(list(var.origin_bucket), concat(list(""), aws_s3_bucket.origin.*.id))))}"
   bucket_domain_name = "${var.use_regional_s3_endpoint == "true" ? format("%s.s3-%s.amazonaws.com" , local.bucket, data.aws_s3_bucket.selected.region): format(var.bucket_domain_format, local.bucket)}"
 }
 
