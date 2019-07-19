4d9847977a3c275f7939177c25f9fc5c5fa70061
Andrew Mackett <andrewm@yoyowallet.com>
2019-07-19 18:27:30 +0100

    var.enabled is a bool not a string

--- main.tf
+++ main.tf
@@ -203,7 +203,7 @@ resource "aws_cloudfront_distribution" "default" {
 
 module "dns" {
   source           = "git::https://github.com/cloudposse/terraform-aws-route53-alias.git?ref=tags/0.3.0"
-  enabled          = var.enabled == "true" && length(var.parent_zone_id) > 0 || length(var.parent_zone_name) > 0 ? "true" : "false"
+  enabled          = var.enabled && length(var.parent_zone_id) > 0 || length(var.parent_zone_name) > 0 ? true : false
   aliases          = var.aliases
   parent_zone_id   = var.parent_zone_id
   parent_zone_name = var.parent_zone_name
