360f4a9e5c95f3a79247e70f7896f1f8a18589d4
Alex Jurkiewicz <alex@jurkiewi.cz>
2021-03-08 13:47:52 +1100

    Improve descriptions of DNS-related variables

--- variables.tf
+++ variables.tf
@@ -246,19 +246,19 @@ variable "geo_restriction_locations" {
 variable "parent_zone_id" {
   type        = string
   default     = ""
-  description = "ID of the hosted zone to contain this record  (or specify `parent_zone_name`)"
+  description = "ID of the hosted zone to contain this record (or specify `parent_zone_name`). Requires `dns_alias_enabled` set to true"
 }
 
 variable "parent_zone_name" {
   type        = string
   default     = ""
-  description = "Name of the hosted zone to contain this record (or specify `parent_zone_id`)"
+  description = "Name of the hosted zone to contain this record (or specify `parent_zone_id`). Requires `dns_alias_enabled` set to true"
 }
 
 variable "dns_alias_enabled" {
   type        = bool
   default     = false
-  description = "Explicitly opt into DNS aliasing the CDN."
+  description = "Create a DNS alias for the CDN. Requires `parent_zone_id` or `parent_zone_name`"
 }
 
 variable "static_s3_bucket" {
