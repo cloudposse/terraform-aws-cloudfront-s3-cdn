c7abc6c2262cc6fa82301b6eeb1134090d007e52
Chris Hudson <chudson@brave.com>
2021-11-10 08:59:53 -0500

    Formatting

--- variables.tf
+++ variables.tf
@@ -371,8 +371,8 @@ variable "ordered_cache" {
     trusted_signers    = list(string)
     trusted_key_groups = list(string)
 
-    cache_policy_id            = string
-    origin_request_policy_id   = string
+    cache_policy_id          = string
+    origin_request_policy_id = string
 
     viewer_protocol_policy = string
     min_ttl                = number
