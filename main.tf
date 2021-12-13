76775e1cba5b6768bf8b240e0b921ca4939ec7ee
Yonatan Koren <me@yonatankoren.com>
2021-12-13 09:34:12 +0200

    Bump route53-alias module version to latest.

--- main.tf
+++ main.tf
@@ -566,7 +566,7 @@ resource "aws_cloudfront_distribution" "default" {
 
 module "dns" {
   source           = "cloudposse/route53-alias/aws"
-  version          = "0.12.0"
+  version          = "0.12.1"
   enabled          = (local.enabled && var.dns_alias_enabled)
   aliases          = var.aliases
   parent_zone_id   = var.parent_zone_id
