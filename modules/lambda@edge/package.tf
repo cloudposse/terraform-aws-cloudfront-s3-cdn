2ab482593b76888e9ce9771497b52c11d94e318e
Yonatan Koren <me@yonatankoren.com>
2021-12-22 00:20:23 +0200

    Update schema to use inline files.

--- modules/lambda@edge/package.tf
+++ modules/lambda@edge/package.tf
@@ -1,10 +1,16 @@
 data "archive_file" "lambda_zip" {
   for_each = local.functions
 
+  dynamic "source" {
+    for_each = each.value.source
+
+    content {
+      content  = source.value.content
+      filename = source.value.filename
+    }
+  }
+
   type             = "zip"
-  source_dir       = each.value.local_path
   output_file_mode = "0666"
   output_path      = "${path.module}/archives/${each.key}.zip"
 }
-
-
--- modules/lambda@edge/variables.tf
+++ modules/lambda@edge/variables.tf
@@ -1,7 +1,10 @@
 variable "functions" {
   description = "TODO"
   type = map(object({
-    local_path   = string
+    source      = list(object({
+      filename = string
+      content  = string
+    }))
     runtime      = string
     handler      = string
     event_type   = string
