07a89921eda8a1103029b42d76d14e2fad6c1e7b
cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>
2021-12-28 13:24:16 +0000

    Auto Format

--- modules/lambda@edge/versions.tf
+++ modules/lambda@edge/versions.tf
@@ -7,11 +7,11 @@ terraform {
       version = ">= 3.0"
     }
     time = {
-      source = "hashicorp/time"
+      source  = "hashicorp/time"
       version = ">= 0.7.0"
     }
     archive = {
-      source = "hashicorp/archive"
+      source  = "hashicorp/archive"
       version = ">= 2.2.0"
     }
   }
