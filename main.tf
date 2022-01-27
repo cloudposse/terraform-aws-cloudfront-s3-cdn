c19ddbd23a9e1cf679c07c1fb1d3e36ff6ca4f25
E Bobo <trutko.artem@gmail.com>
2022-01-27 19:03:49 +0300

    Added new response_headers_policy_id argument for a block with Custom behavior (#211)
    Co-authored-by: cloudpossebot <11232728+cloudpossebot@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -549,10 +549,11 @@ resource "aws_cloudfront_distribution" "default" {
         }
       }
 
-      viewer_protocol_policy = ordered_cache_behavior.value.viewer_protocol_policy
-      default_ttl            = ordered_cache_behavior.value.default_ttl
-      min_ttl                = ordered_cache_behavior.value.min_ttl
-      max_ttl                = ordered_cache_behavior.value.max_ttl
+      viewer_protocol_policy     = ordered_cache_behavior.value.viewer_protocol_policy
+      default_ttl                = ordered_cache_behavior.value.default_ttl
+      min_ttl                    = ordered_cache_behavior.value.min_ttl
+      max_ttl                    = ordered_cache_behavior.value.max_ttl
+      response_headers_policy_id = ordered_cache_behavior.value.response_headers_policy_id
 
       dynamic "lambda_function_association" {
         for_each = ordered_cache_behavior.value.lambda_function_association
