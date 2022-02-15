0c2fce6b803803b74e6a0fa96b3ce4e573f15673
Mauricio Wyler <mwyler@ebowe.com>
2022-02-15 15:45:34 -0300

    Update versions.tf
    response_headers_policy_id first appeared on version: 3.64.0 (November 04, 2021)
    As of version 4.0.0 (February 10, 2022), many breaking changes affect this module.
    
    We need to restrict it until rewritten.

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.41.0"
+      version = ">= 3.64.0, < 4.0.0"
     }
     random = {
       source  = "hashicorp/random"
