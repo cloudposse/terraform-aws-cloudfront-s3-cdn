f538f63d3254dab0070f5fa245dca0c6e4710f04
David Howell <david@davidjameshowell.com>
2022-05-06 04:49:03 +0000

    Bump AWS provider to 4.5.0 and Terraform to 1.0.0

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.0.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.64.0, < 4.0.0"
+      version = ">= 4.5.0"
     }
     random = {
       source  = "hashicorp/random"
