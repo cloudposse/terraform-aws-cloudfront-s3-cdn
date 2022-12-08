4dbfb0352652850b98a62932ab5c49e3a6aba11a
Paul van den Burg <1727604+paulvandenburg@users.noreply.github.com>
2022-12-08 16:44:35 +0100

    Upgrade cloudposse/s3-log-storage/aws to 1.0.0

--- versions.tf
+++ versions.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = ">= 0.13.0"
+  required_version = ">= 1.3.0"
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.64.0, != 4.0.0, != 4.1.0, != 4.2.0, != 4.3.0, != 4.4.0, != 4.5.0, != 4.6.0, != 4.7.0, != 4.8.0"
+      version = ">= 4.9.0"
     }
     random = {
       source  = "hashicorp/random"
