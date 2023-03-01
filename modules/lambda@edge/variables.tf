b704e0e067a593b7a6db67f9b379f1ba03915da2
Zinovii Dmytriv <zinovii@gmail.com>
2023-03-01 15:37:41 +0200

    update

--- modules/lambda@edge/variables.tf
+++ modules/lambda@edge/variables.tf
@@ -19,7 +19,7 @@ variable "functions" {
       filename = string
       content  = string
     }))
-    source_dir   = list(string)
+    source_dir   = string
     runtime      = string
     handler      = string
     event_type   = string
