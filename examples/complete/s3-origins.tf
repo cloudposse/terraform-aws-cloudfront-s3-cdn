c5f58dd672f37d91434b64f6eaccfe2bc2c41361
milldr <miller0daniel@gmail.com>
2023-07-21 17:34:27 -0700

    set s3_object_ownership for test buckets

--- examples/complete/s3-origins.tf
+++ examples/complete/s3-origins.tf
@@ -32,11 +32,11 @@ module "additional_s3_origin" {
   version = "3.1.2"
   enabled = local.additional_s3_origins_enabled
 
-  acl                = "private"
-  force_destroy      = true
-  user_enabled       = false
-  versioning_enabled = false
-  attributes         = ["s3"]
+  s3_object_ownership = "BucketOwnerEnforced"
+  force_destroy       = true
+  user_enabled        = false
+  versioning_enabled  = false
+  attributes          = ["s3"]
 
   context = module.this.context
 }
@@ -46,11 +46,11 @@ module "additional_s3_failover_origin" {
   version = "3.1.2"
   enabled = local.additional_s3_origins_enabled
 
-  acl                = "private"
-  force_destroy      = true
-  user_enabled       = false
-  versioning_enabled = false
-  attributes         = ["s3", "fo"] # fo = failover
+  s3_object_ownership = "BucketOwnerEnforced"
+  force_destroy       = true
+  user_enabled        = false
+  versioning_enabled  = false
+  attributes          = ["s3", "fo"] # fo = failover
 
   context = module.this.context
 }
