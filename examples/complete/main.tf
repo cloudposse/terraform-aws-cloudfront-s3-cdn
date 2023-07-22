7f5a8b463f27b86f17645a57d378160b14567e68
milldr <miller0daniel@gmail.com>
2023-07-21 18:28:36 -0700

    set ownership on test bucket

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -38,12 +38,14 @@ module "s3_bucket" {
   source  = "cloudposse/s3-bucket/aws"
   version = "3.1.2"
 
-  acl                 = "log-delivery-write"
   force_destroy       = true
   user_enabled        = false
   versioning_enabled  = false
   block_public_policy = false
   attributes          = ["existing-bucket"]
+
+  s3_object_ownership = "ObjectWriter"
+  acl                 = "log-delivery-write"
 
   context = module.this.context
 }
