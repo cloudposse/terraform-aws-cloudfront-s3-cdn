7f7ed7ae144fc04bd86293cfe3ae309c388b8941
Jakub Wądołowski <6334715+jwadolowski@users.noreply.github.com>
2025-02-24 12:55:26 +0100

    fix: Make sure tags are associated with Lambda functions

--- modules/lambda@edge/main.tf
+++ modules/lambda@edge/main.tf
@@ -84,6 +84,7 @@ resource "aws_lambda_function" "default" {
   filename         = each.value.source_zip != null ? data.local_file.lambda_zip[each.key].filename : data.archive_file.lambda_zip[each.key].output_path
   source_code_hash = each.value.source_zip != null ? sha256(data.local_file.lambda_zip[each.key].content_base64) : data.archive_file.lambda_zip[each.key].output_base64sha256
   publish          = true
+  tags             = module.function_label[each.key].tags
 }
 
 resource "aws_lambda_permission" "allow_cloudfront" {
@@ -94,3 +95,4 @@ resource "aws_lambda_permission" "allow_cloudfront" {
   action        = "lambda:GetFunction"
   principal     = "edgelambda.amazonaws.com"
 }
+
