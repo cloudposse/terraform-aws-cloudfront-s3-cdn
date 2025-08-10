cc2de2387cffd8f7e3f21fe570919f96526ef768
Jakub Wądołowski <6334715+jwadolowski@users.noreply.github.com>
2025-08-10 17:47:29 +0200

    fix: Distinguish minimal s3-cdn module instance from the primary one
    There are 2 s3-cdn module instances - in `examples/complete/main.tf` and
    `examples/complete/minimal.tf` files respectively. To avoid name
    collisions, let's add an attribute that differentiates them

--- examples/complete/minimal.tf
+++ examples/complete/minimal.tf
@@ -5,6 +5,9 @@ module "minimal" {
   stage     = var.stage
   name      = var.name
 
+  // This is required to distinguish this module instance from the one in main.tf and to prevent S3 bucket name collisions
+  attributes = concat(var.attributes, ["minimal"])
+
   cloudfront_access_logging_enabled = false
 
   context = module.this.context
