365753db3ff373cd5c34ca8f8ad4e56346465d79
Jakub Wądołowski <6334715+jwadolowski@users.noreply.github.com>
2025-09-02 00:38:54 +0200

    fix: Resolve unsupported attribute error in S3 website block
    Resolves #354

---
+++ examples/complete/website.tf
@@ -0,0 +1,34 @@
+module "website_default" {
+  source = "../../"
+
+  namespace = var.namespace
+  stage     = var.stage
+  name      = var.name
+
+  // Distinguish this module instance from the one in main.tf and to prevent S3 bucket name collisions
+  attributes = concat(var.attributes, ["website"])
+
+  cloudfront_access_logging_enabled = false
+
+  website_enabled = true
+
+  context = module.this.context
+}
+
+module "website_redirect_all" {
+  source = "../../"
+
+  namespace = var.namespace
+  stage     = var.stage
+  name      = var.name
+
+  // Distinguish this module instance from the one in main.tf and to prevent S3 bucket name collisions
+  attributes = concat(var.attributes, ["website-redirect-all"])
+
+  cloudfront_access_logging_enabled = false
+
+  website_enabled          = true
+  redirect_all_requests_to = "https://cloudposse.com"
+
+  context = module.this.context
+}
